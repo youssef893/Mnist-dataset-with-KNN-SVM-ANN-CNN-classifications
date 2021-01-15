794c8df0403715afaba97bafb449b569c4e9e9c4
youssef893 <youssefsayed893@gmail.com>
2021-01-15 21:27:19 +0200

    Update RunModels.py

--- RunModels.py
+++ RunModels.py
@@ -7,8 +7,7 @@ from getFeatures import GetFeatures
 class RunModels:
 
     @staticmethod
-    def printAccuracy(predictionList, labelsList, accuracy):
-        print("Accuracy =", accuracy, "%")
+    def printAccuracy(predictionList, labelsList):
         print("Accuracy_Score =", accuracy_score(labelsList, predictionList) * 100, "%")
         print("R2_score =", r2_score(labelsList, predictionList) * 100, "%")
         print("Confusion matrix =", confusion_matrix(labelsList, predictionList))
@@ -16,28 +15,20 @@ class RunModels:
         print("Recall =", recall_score(labelsList, predictionList, average=None) * 100)
         print("Classification Report =", classification_report(labelsList, predictionList))
 
-    def calculateAccuracy(self, predictionList, labelsList):
-        counter = 0
-        for i in range(len(predictionList)):
-            if predictionList[i] == labelsList[i]:
-                counter += 1
-        accuracy = counter / len(predictionList) * 100
-        self.printAccuracy(predictionList, labelsList, accuracy)
-
     def calculateModelsAccuracy(self, KnnPrediction, SvmPrediction,
                                 ANNPrediction, CNNPrediction, testLabels):
         print('Wait for calculating accuracy...')
         print("KNN Model:")
-        self.calculateAccuracy(KnnPrediction, testLabels)
+        self.printAccuracy(KnnPrediction, testLabels)
         print("==============================================================")
         print("Svm Model:")
-        self.calculateAccuracy(SvmPrediction, testLabels)
+        self.printAccuracy(SvmPrediction, testLabels)
         print("==============================================================")
         print("ANN Model:")
-        self.calculateAccuracy(ANNPrediction, testLabels)
+        self.printAccuracy(ANNPrediction, testLabels)
         print("==============================================================")
         print("CNN Model:")
-        self.calculateAccuracy(CNNPrediction, testLabels)
+        self.printAccuracy(CNNPrediction, testLabels)
 
     def applyModels(self, trainFeatures, trainLabels, testFeatures, testLabels):
         hogTrainFeatures, hogTrainImages = GetFeatures.extractHOGFeatures(trainFeatures)
@@ -45,10 +36,11 @@ class RunModels:
         # Plot first hog image
         print('Hog is applied and one image was plotted')
         GetFeatures.plotImage(hogTrainImages[0], "hog")
-        KnnPrediction = Models.applyKNN(hogTrainFeatures, trainLabels, hogTestFeatures)
-        SvmPrediction = Models.applySVM(hogTrainFeatures, trainLabels, hogTestFeatures)
-        ANNPrediction = Models.applyANN(hogTrainFeatures, trainLabels, hogTestFeatures)
-        CNNPrediction = Models.applyCNN(trainFeatures, trainLabels, testFeatures, testLabels)
+        model = Models()
+        KnnPrediction = model.applyKNN(hogTrainFeatures, trainLabels, hogTestFeatures)
+        SvmPrediction = model.applySVM(hogTrainFeatures, trainLabels, hogTestFeatures)
+        ANNPrediction = model.applyANN(hogTrainFeatures, trainLabels, hogTestFeatures)
+        CNNPrediction = model.applyCNN(trainFeatures, trainLabels, testFeatures, testLabels)
         # Calculate accuracy of models on test data
         self.calculateModelsAccuracy(KnnPrediction, SvmPrediction,
                                      ANNPrediction, CNNPrediction, testLabels)
