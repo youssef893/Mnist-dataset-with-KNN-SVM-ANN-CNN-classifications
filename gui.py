1989d29dcd10e44770b01d020238f2b160d40739
youssef893 <youssefsayed893@gmail.com>
2021-01-15 21:27:08 +0200

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,80 @@
+from keras.models import load_model
+from tkinter import *
+import tkinter as tk
+import win32gui
+from PIL import ImageGrab, Image
+import numpy as np
+
+model = load_model('cnn.h5')
+
+
+def predict_digit(img):
+    # resize image to 28x28 pixels
+    img = img.resize((28, 28))
+    # convert rgb to grayscale
+    img = img.convert('L')
+    img = np.array(img)
+    # reshaping to support our model input and normalizing
+    img = img.reshape(1, 28, 28, 1)
+    # predicting the class
+    res = model.predict([img])
+    print(res)
+    print(max(res))
+    print(np.argmax(res, axis=1))
+    return np.argmax(res, axis=1), max(res)
+
+
+class App(tk.Tk):
+
+    cnn = 0
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.x = self.y = 0
+
+        # Creating elements
+        self.canvas = tk.Canvas(self, width=300, height=300, bg="purple", cursor="cross")
+        self.label = tk.Label(self, text="Thinking..", font=("Helvetica", 48))
+        self.classify_btn = tk.Button(self, text="Recognise", command=self.classify_handwriting)
+        self.button_clear = tk.Button(self, text="Clear", command=self.clear_all)
+
+        # Grid structure
+        self.canvas.grid(row=0, column=0, pady=2, sticky=W, )
+        self.label.grid(row=0, column=1, pady=2, padx=2)
+        self.classify_btn.grid(row=1, column=1, pady=2, padx=2)
+        self.button_clear.grid(row=1, column=0, pady=2)
+
+        # self.canvas.bind("<Motion>", self.start_pos)
+        self.canvas.bind("<B1-Motion>", self.draw_lines)
+
+    def clear_all(self):
+        self.canvas.delete("all")
+        self.label.configure(text='')
+
+    @staticmethod
+    def getNumber(digit):
+        numbersList = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+        for i in range(len(numbersList)):
+            if i == digit:
+                return numbersList[i]
+
+    def classify_handwriting(self):
+        HWND = self.canvas.winfo_id()  # get the handle of the canvas
+        rect = win32gui.GetWindowRect(HWND)  # get the coordinate of the canvas
+        im = ImageGrab.grab(rect)
+
+        digit, acc = predict_digit(im)
+
+        self.label.configure(text=self.getNumber(digit))
+
+    def draw_lines(self, event):
+        self.x = event.x
+        self.y = event.y
+        r = 8
+        self.canvas.create_oval(self.x - r, self.y - r, self.x + r, self.y + r, fill='yellow')
+
+
+app = App()
+mainloop()
+
