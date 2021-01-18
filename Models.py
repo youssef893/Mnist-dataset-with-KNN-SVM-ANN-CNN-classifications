d2b73a76828e8644143fd0af2e1b1f5779c81087
youssef893 <youssefsayed893@gmail.com>
2021-01-18 13:26:26 +0200

    Update Models.py

--- Models.py
+++ Models.py
@@ -61,7 +61,7 @@ class Models:
     def compileCnn(cnn, trainFeatures, trainLabels, testFeatures, testLabels):
         # compile cnn
         cnn.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-        cnn.fit(trainFeatures.reshape(60000, 28, 28, 1), to_categorical(trainLabels), epochs=10, batch_size=128,
+        cnn.fit(trainFeatures.reshape(60000, 28, 28, 1), to_categorical(trainLabels), epochs=15, batch_size=128,
                 validation_data=(testFeatures.reshape(10000, 28, 28, 1), to_categorical(testLabels)))
         prediction = cnn.predict(testFeatures.reshape(10000, 28, 28, 1))
         score = cnn.evaluate(testFeatures.reshape(10000, 28, 28, 1), to_categorical(testLabels), verbose=0)
@@ -72,13 +72,13 @@ class Models:
     def applyCNN(self,trainFeatures, trainLabels, testFeatures, testLabels):
         cnn = Sequential()
         # first conv layer
-        cnn.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu", input_shape=(28, 28, 1)))
+        cnn.add(Conv2D(filters=32, kernel_size=(3, 3), activation="relu", input_shape=(28, 28, 1)))
         # second layer
         cnn.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu"))  # filters is number of filters & kernel size is size of filter
         # max pooling layer
         cnn.add(MaxPooling2D(pool_size=(2, 2)))
         cnn.add(Conv2D(filters=128, kernel_size=(3, 3), activation="relu"))
-        cnn.add(Conv2D(filters=128, kernel_size=(3, 3), activation="relu"))
+        cnn.add(Conv2D(filters=256, kernel_size=(3, 3), activation="relu"))
         cnn.add(MaxPooling2D(pool_size=(2, 2)))
         cnn.add(Conv2D(filters=256, kernel_size=(3, 3), activation="relu"))
         cnn.add(MaxPooling2D(pool_size=(2, 2)))
